1253471305c10909f6ff4b4945fb3f404d9fddfd
Mrina Sugosh <mrina.sugosh@gmail.com>
2024-12-10 01:38:17 -0500

    setup

--- src/components/TinyEditor.jsx
+++ src/components/TinyEditor.jsx
@@ -1,25 +1,5 @@
 import { Editor } from '@tinymce/tinymce-react';
 
-import {
-  advtemplate_list,
-  advtemplate_get_template,
-  advtemplate_create_category,
-  advtemplate_create_template,
-  advtemplate_rename_category,
-  advtemplate_rename_template,
-  advtemplate_delete_template,
-  advtemplate_delete_category,
-  advtemplate_move_template,
-  advtemplate_move_category_items,
-  mergetags_list,
-} from './template_functions';
-
-import { ai_request } from './ai_functions';
-import { currentUser } from './servergen';
-import { mentions_fetch, mentions_menu_hover, mentions_menu_complete, mentions_select, tinycomments_can_resolve } from './comments_functions';
-
-import '../styles/mergetags.css';
-
 export default function TinyEditor({ editorRef }) {
   return (
     <>
@@ -28,54 +8,10 @@ export default function TinyEditor({ editorRef }) {
       initialValue=""
       onInit={(_evt, editor) => editorRef.current = editor}
       init={{
-        plugins: 'advcode tinycomments mentions advtemplate ai anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker markdown mergetags',
-        toolbar: 'mergetags | inserttemplate addtemplate | aidialog aishortcuts | undo redo | addcomment showcomments | blocks fontfamily fontsize | bold italic underline strikethrough | code link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
-        menu: {
-          tc: {
-            title: 'TinyComments',
-            items: 'addcomment showcomments deleteallconversations'
-          }
-        },
+        plugins: 'code anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount linkchecker markdown mergetags',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | code link image media table | align lineheight | numlist bullist indent outdent | emoticons charmap | removeformat',
 
         height: 800,
-
-        advcode_inline: true,
-
-        tinycomments_mentions_enabled: true,
-        readonly: true,
-        mentions_item_type: 'profile',
-        mentions_min_chars: 0,
-        mentions_selector: '.mymention',
-        mentions_fetch,
-        mentions_menu_hover,
-        mentions_menu_complete,
-        mentions_select,
-        
-
-        tinycomments_author: currentUser.id,
-        tinycomments_author_name: currentUser.fullName,
-        tinycomments_avatar: currentUser.image,
-        tinycomments_mode: 'embedded',
-        sidebar_show: 'showcomments',
-        tinycomments_can_resolve,
-
-        contextmenu: 'advtemplate',
-        advtemplate_list,
-        advtemplate_get_template,
-        advtemplate_create_category,
-        advtemplate_create_template,
-        advtemplate_rename_category,
-        advtemplate_rename_template,
-        advtemplate_delete_template,
-        advtemplate_delete_category,
-        advtemplate_move_template,
-        advtemplate_move_category_items,
-        mergetags_prefix: '{{',
-        mergetags_suffix: '}}',
-        mergetags_list,
-        content_css: '/src/styles/mergetags.css',
-
-        ai_request,
       }}
     />
     </>
